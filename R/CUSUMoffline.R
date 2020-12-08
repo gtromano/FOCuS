ee6d42c7c7cc57a8572f443402a02bd1de656aef
gtromano <g.romano@lancaster.ac.uk>
2020-12-08 16:46:39 +0100

    added CUSUM and MOSUM methods

---
+++ R/CUSUMoffline.R
@@ -0,0 +1,27 @@
+.CUSUMstep <- function (S, new_point, m) {
+
+  S <- S + .H(new_point, m)
+  return(S)
+}
+
+.H <- function (x, m) (x - m)
+
+CUSUM_offline <- function (Y, threshold, m = 0) {
+
+  S <- 0
+  cp <- -1
+  t <- 0
+
+  for (y in Y) {
+    t <- t + 1
+    S <- .CUSUMstep(S, y, m)
+    if (abs(S) >= threshold) {
+      cp <- t
+      break
+    }
+  }
+  return(cp)
+}
+
+
+CUSUM_offline(c(rnorm(100), rnorm(100, -2)), 15, 0)
