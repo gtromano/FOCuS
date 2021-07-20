b87af3995d125fe02ce0b4bbf5ac06b42eb413d5
gtromano <g.romano@lancaster.ac.uk>
2021-07-20 16:16:10 +0200

    fixed sims bug

--- simulations/pre-change-known/detection_rate_NEW.R
+++ simulations/pre-change-known/detection_rate_NEW.R
@@ -67,7 +67,7 @@ tlist <- apply(avg_run_len, 2, function (len) row.names(avg_run_len)[which(len =
 if (T) {
   NREP <- 100
   set.seed(42)
-  noise <- mclapply(1:NREP, function (i) rnorm(N), mc.cores = CORES)
+  noise <- mclapply(1:NREP, function (i) rnorm(2e6), mc.cores = CORES)
   #run_simulation(sim_grid[10, ], NREP, noise, tlist = tlist)
   outDF <- lapply(seq_len(nrow(sim_grid)), function (i) {
     p <- sim_grid[i, ]
