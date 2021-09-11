573574cb4bb6c3022a31ca9413cada3a109a9438
gtromano <g.romano@lancaster.ac.uk>
2021-09-11 18:28:08 +0100

    updated simulations

--- simulations/pre-change-unknown/detection_rate.R
+++ simulations/pre-change-unknown/detection_rate.R
@@ -7,7 +7,8 @@ run_simulation <- function(p, REPS, seed = 42, tlist) {
   #grid <- find_grid(0, 50, .01, 1.3)
 
   set.seed(seed)
-  means <- runif(REPS, 1, 10)
+  #means <- runif(REPS, 1, 10)
+  means <- rep(0, REPS)
   data <- lapply(1:REPS, function (k) c(rnorm(1e5 + p$changepoint, means[k]), rnorm(p$N - p$changepoint, means[k] + p$delta)))
 
   # FOCuS with no pruning constraint
@@ -50,10 +51,10 @@ run_simulation <- function(p, REPS, seed = 42, tlist) {
 
 
 
-output_file <- "./simulations/pre-change-unknown/results/dr_ukn8.RData"
+output_file <- "./simulations/pre-change-unknown/results/dr_ukn9.RData"
 
 sim_grid <- expand.grid(
-  N = 4e6,
+  N = 5e6,
   changepoint = 1e5,
   delta = 0.5 ^ seq(8,0, length.out = 20)
 )
@@ -64,7 +65,7 @@ load("simulations/pre-change-unknown/tlist.RData")
 #tlist[3,1] <- 12.5
 
 #run_simulation(sim_grid[10, ], 10, tlist = tlist) # test run
-if (T) {
+if (F) {
   NREP <- 100
   outDF <- lapply(seq_len(nrow(sim_grid)), function (i) {
     p <- sim_grid[i, ]
@@ -81,55 +82,42 @@ load(output_file)
 summary_df <- outDF %>% mutate(
     run_len = if_else(est == -1, N, est),
     no_detection = if_else(est == -1, 1, 0),
-    det_delay = ifelse(est - real >= 0, est - real, N - real),
-    false_alarm = if_else(!no_detection & (det_delay == N - real), 1, 0),
+    false_alarm = if_else(!no_detection & (est - real < 0), 1, 0),
+    det_delay = ifelse((est - real >= 0) & (!false_alarm), est - real, NA),
     true_positive = if_else(!no_detection & !false_alarm,1, 0), # if it's not a missed detection nor it's a false alarm, then it's a true positive
   )
 
 
-grouped <- summary_df %>% group_by(algo) %>%
+# plug N minus the changepoint in case we get a missed detection!
+summary_df[(summary_df$no_detection) == 1, "det_delay"] <- sim_grid$N[1] - sim_grid$changepoint[1]
+
+
+###### checks ########
+summary_df %>% filter(algo == "FOCuS", no_detection == 1, magnitude < .004)
+summary_df %>% filter(algo == "FOCuS0 1e+05", no_detection == 1, magnitude < .004)
+######################
+
+
+grouped <- summary_df %>% group_by(magnitude, algo) %>%
   summarise(no_detection = mean(no_detection), false_alarm = mean(false_alarm), tp_rate = mean(true_positive), det_del = mean(det_delay, na.rm = T))
-print(grouped, n = 80)
-
-
-### false alarm rate ####
-
-cbPalette <- RColorBrewer::brewer.pal(6, "Paired")[c(2, 1, 3, 4, 5, 6)]
-fa_rate <- ggplot(summary_df %>% filter(algo != "MOSUM"), aes(x = magnitude, y = false_alarm, group = algo, col = algo)) +
-  stat_summary(fun.data = "mean_se", geom = "line") +
-#  stat_summary(fun.data = "mean_se", geom = "errorbar") +
-  scale_color_manual(values = cbPalette) +
-  ylim(0, 1) +
-  xlab("magnitude") +
-  ylab("False Alarm Rate") +
-  theme_idris()
-
-fa_rate
-
-tp_rate <- ggplot(summary_df %>% filter(algo != "MOSUM"), aes(x = magnitude, y = true_positive, group = algo, col = algo)) +
-  stat_summary(fun.data = "mean_se", geom = "line") +
-#  stat_summary(fun.data = "mean_se", geom = "errorbar") +
-  scale_color_manual(values = cbPalette) +
-  ylim(0, 1) +
-  xlab("magnitude") +
-  ylab("True Positive Rate") +
-  theme_idris()
-
-tp_rate
+print(grouped, n = 200)
 
 
 ### detection delay ####
+cbPalette <- RColorBrewer::brewer.pal(6, "Paired")[c(2,1, 3, 4, 5, 6)]
+
 detection_delay <-
   ggplot(
-    summary_df,
+    #grouped ,
+    grouped %>% filter(algo != "FOCuS-t"),
     aes(
       x = magnitude,
-      y = det_delay,
+      y = det_del,
       group = algo,
       col = algo
     )
   ) +
-  stat_summary(fun.data = "mean_se", geom = "line") +
+    geom_line() +
   scale_color_manual(values = cbPalette) +
   xlab("magnitude") +
   ylab("Detection Delay") +
@@ -139,42 +127,42 @@ detection_delay <-
 detection_delay
 
 
-ggsave(filename = "simulations/pre-change-unknown/results/2-det-delay.pdf", ggarrange(detection_delay, legend = F), width = 6, height = 6)
-
-#tot_dr <- ggarrange(fa_rate, tp_rate, detection_delay, labels = "AUTO", nrow = 3, common.legend = T, legend = "right")
-ggsave("simulations/pre-change-unknown/results/dr.pdf", tot_dr, width = 6, height = 10)
-
-tot_sim <- ggarrange(avg_run_len_plot, detection_delay, nrow = 1, common.legend = T, legend = "right", labels = "AUTO")
-ggsave("simulations/pre-change-unknown/results/tot_sim_ukn.pdf", tot_sim, width = 15, height = 7)
-
-#########################
-# algo1 <- "FOCuS"
-# algo2 <- "Page-CUSUM 5"
-
-# detection_diff <- (summary_df %>% filter(algo == algo1))$est - (summary_df %>% filter(algo == algo2))$est
-#
-# summary2 <- summary_df %>% filter(algo == algo1) %>%
-#   mutate(diff = detection_diff)
-#
-#
-#  ggplot(summary2 %>% filter(true_positive == 1),
-#                            aes(x = magnitude, y = diff)) +
-#   stat_summary(fun.data = "mean_se", geom = "line") +
-#   stat_summary(fun.data = "mean_se", geom = "errorbar") +
-#   scale_color_manual(values = cbPalette) +
-#    geom_hline(yintercept = 0, lty = 2, colour = "grey") +
-#   xlab("magnitude") +
-#   ylab("Detection delay between FOCuS 5 and Page-CUSUM 5") +
-#   theme_idris()
+fp <-
+  ggplot(
+    #grouped ,
+    grouped %>% filter(algo != "FOCuS-t"),
+    aes(
+      x = magnitude,
+      y = false_alarm,
+      group = algo,
+      col = algo
+    )
+  ) +
+    geom_line() +
+  scale_color_manual(values = cbPalette) +
+  xlab("magnitude") +
+  ylab("Detection Delay") +
+  scale_y_log10() +
+  scale_x_log10() +
+  theme_idris() #+ theme(legend.position = "none")
+fp
 
 
-test <- (summary_df %>% filter(algo == "FOCuS"))$det_delay - (summary_df %>% filter(algo == "FOCuS t1000"))$det_delay
-mean(test, na.rm = T)
 
-test <- (summary_df %>% filter(magnitude > .1, algo == "FOCuS"))$det_delay - (summary_df %>% filter(magnitude > .1, algo == "FOCuS t1000"))$det_delay
-mean(test, na.rm = T)
+##### ratio plot ####
 
-test <- (summary_df %>% filter(magnitude < .1, algo == "FOCuS"))$det_delay - (summary_df %>% filter(magnitude < .1, algo == "FOCuS t1000"))$det_delay
-mean(test, na.rm = T)
+det_del_table <- summary_df %>% filter(magnitude > 0, magnitude < 2) %>% group_by(magnitude, algo) %>% summarise(dd = mean(det_delay, na.rm = T), no_det = mean(no_detection, na.rm = T), fa = mean(false_alarm, na.rm = T))
+print(det_del_table, n = 100)
 
+summ_table <- pivot_wider(det_del_table[1:3], names_from = algo, values_from = dd) #%>% mutate(FOCuSvPage = FOCuS0 - `Page-20p`, FOCuSvMOSUM = FOCuS0 - MOSUM) %>%  print(n = 100)
+summ_table
 
+comp_table <- summ_table %>% mutate(FOCuSvFOCuS0 = FOCuS / `FOCuS-t`)
+
+ggplot(comp_table) +
+        geom_hline(yintercept = 0, col = "grey", lty = 2) +
+        geom_line(aes(x = magnitude, y = log(FOCuSvFOCuS0))) +
+        scale_x_log10() +
+        ylim(-1, 1) +
+        theme_idris() +
+        theme(axis.title=element_blank())
