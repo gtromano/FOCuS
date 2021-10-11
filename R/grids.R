d9ebc16cc621085531c6bcb60b2686da144e2313
gtromano <g.romano@lancaster.ac.uk>
2021-10-11 12:32:29 +0100

    improved documentation

--- R/grids.R
+++ R/grids.R
@@ -1,15 +1,21 @@
 #' Compute a geometric grid of values.
 #'
-#' @param mu0 
-#' @param max_ints 
-#' @param smallest_mag 
-#' @param g 
+#' @param mu0 The center of the grid, corresponding to the pre-change location.
+#' @param max_ints The maximum amounts of intervals to generate.
+#' @param smallest_mag The smallest absolute difference from the center of the grid.
+#' @param g Parameter to regulate the geometric increase of the grid.
 #'
-#' @return
+#' @return A vector of grid values.
 #' @export
 #'
 #' @examples
-find_grid <- function(mu0 = 0, max_ints = 20, smallest_mag = .001, g = .5) {
+#'
+#' set.seed(42)
+#' y <- c(rnorm(3e5, 0), rnorm(1e4, 1))
+#' g <- find_grid()
+#' FOCuS(y, 18, mu0 = 0, grid = g)
+
+find_grid <- function(mu0 = 0, max_ints = 21, smallest_mag = .01, g = 1.74) {
   
   interval_lengths <- vector(length = max_ints/2)
   interval_lengths[1] <- smallest_mag
