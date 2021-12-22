011b24a8a9b98971562d70bb7fc58357a340176f
gtromano <g.romano@lancaster.ac.uk>
2021-12-22 12:50:25 +0000

    update sim

--- simulations/pre-change-known/avg_run_len2.R
+++ simulations/pre-change-known/avg_run_len2.R
@@ -43,7 +43,7 @@ if (T) {
   }, mc.cores = CORES)
 
 
-  save(simRUN, file = "simulations/pre-change-known/results/avg_run_len_NEW7.RData")
+  save(simRUN, file = "simulations/pre-change-known/results/avg_run_len_NEW8.RData")
 
 }
 
@@ -51,7 +51,7 @@ cat("All done\n")
 
 
 
-#load("simulations/pre-change-known/results/avg_run_len_NEW7.RData")
+#load("simulations/pre-change-known/results/avg_run_len_NEW8.RData")
 
 
 # checking broken sims
